2fba6e1920e15a4e9d0945fa88124fd3252eac89
Andrew Yap <andrew@yconsult.com.au>
2014-01-06 17:15:21 +1100

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -2,4 +2,9 @@ require 'erb'
 
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
+template2 = ERB.new "The value of x + 2 is: <%= x + 2 %>"
+template3 = ERB.new "The value of x * 2 is: <%= x * 2 %>"
+
 puts template.result(binding)
+puts template2.result(binding)
+puts template3.result(binding)
